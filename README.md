47d0182ed077689846082aeccc427221ea43ef53
Jin Young Bang <jybang@bu.edu>
2019-03-13 09:53:14 -0400

    deleted all old files

--- README.md
+++
@@ -1,3 +0,0 @@
-# jinyoungbang.github.io
-
-This is my personal website!
--- app.js
+++
@@ -1,6 +0,0 @@
-$(".btn").click(function() {
-	var href = $(this).attr('href');
-        $('html, body').animate({
-            scrollTop: $(href).offset().top
-        }, 100);
-    });
